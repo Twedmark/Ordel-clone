63788ac77ad434e7fc2d4425b184154b3a206e81
Twedmark <axel.twedmark@hotmail.com>
2024-03-19 16:09:44 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ const {
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const BASE_URL = process.env.ORIGIN || "https://localhost:3000";
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.header("Access-Control-Allow-Origin", BASE_URL);
   res.header("Access-Control-Allow-Methods", "GET, POST");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
