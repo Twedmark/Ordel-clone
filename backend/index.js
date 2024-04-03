2f8a323716faed9d6e3c0ff5ce3a1b377b64b600
Twedmark <axel.twedmark@hotmail.com>
2024-04-03 16:28:35 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const { getCurrentRound, allowedWord } = require("../db");
-const { newRound } = require("../cron");
+const { getCurrentRound, allowedWord } = require("./db");
+const { newRound } = require("./cron");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
