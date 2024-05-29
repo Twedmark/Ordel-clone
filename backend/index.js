7386b5ced99b976944e668a967df2888e5c99cd4
Twedmark <axel.twedmark@hotmail.com>
2024-05-29 13:23:27 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,127 +1,127 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const { getCurrentRound, allowedWord, newRound } = require("./db");
-const cron = require("node-cron");
+// const express = require("express");
+// const cors = require("cors");
+// const { getCurrentRound, allowedWord, newRound } = require("./db");
+// const cron = require("node-cron");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(express.json());
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
 
-app.get("/", (req, res) => {
-  res.json({ success: true });
-});
+// app.get("/", (req, res) => {
+//   res.json({ success: true });
+// });
 
-app.get("/api/word", async (req, res) => {
-  console.log("GET /api/word");
-  const round = await getCurrentRound();
-  // setTimeout(async () => {
-  res.json(round);
-  // }, 3000);
-});
+// app.get("/api/word", async (req, res) => {
+//   console.log("GET /api/word");
+//   const round = await getCurrentRound();
+//   // setTimeout(async () => {
+//   res.json(round);
+//   // }, 3000);
+// });
 
-app.get("/api/allowedWord/:word", async (req, res) => {
-  console.log("get /api/allowedWord");
+// app.get("/api/allowedWord/:word", async (req, res) => {
+//   console.log("get /api/allowedWord");
 
-  const responseObj = {
-    success: false,
-    allCorrect: false,
-    guess: req.params.word.split(""),
-    result: Array(5).fill(""),
-  };
+//   const responseObj = {
+//     success: false,
+//     allCorrect: false,
+//     guess: req.params.word.split(""),
+//     result: Array(5).fill(""),
+//   };
 
-  // const wait = (t) => new Promise((resolve, reject) => setTimeout(resolve, t));
+//   // const wait = (t) => new Promise((resolve, reject) => setTimeout(resolve, t));
 
-  // const wait2sec = await wait(6000);
+//   // const wait2sec = await wait(6000);
 
-  try {
-    const allowedPromise = new Promise((resolve, reject) => {
-      allowedWord(req.params.word)
-        .then((result) => {
-          result ? resolve(true) : reject(false);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+//   try {
+//     const allowedPromise = new Promise((resolve, reject) => {
+//       allowedWord(req.params.word)
+//         .then((result) => {
+//           result ? resolve(true) : reject(false);
+//         })
+//         .catch((error) => {
+//           reject(error);
+//         });
+//     });
 
-    const [currentRound, isAllowed] = await Promise.all([
-      getCurrentRound(),
-      allowedPromise,
-    ]);
+//     const [currentRound, isAllowed] = await Promise.all([
+//       getCurrentRound(),
+//       allowedPromise,
+//     ]);
 
-    if (isAllowed) {
-      responseObj.success = true;
+//     if (isAllowed) {
+//       responseObj.success = true;
 
-      let correctWord = currentRound.word;
+//       let correctWord = currentRound.word;
 
-      for (let i = 0; i < 5; i++) {
-        const guessChar = responseObj.guess[i].toUpperCase();
-        const correctChar = correctWord[i].toUpperCase();
+//       for (let i = 0; i < 5; i++) {
+//         const guessChar = responseObj.guess[i].toUpperCase();
+//         const correctChar = correctWord[i].toUpperCase();
 
-        if (guessChar === correctChar) {
-          responseObj.result[i] = "C";
-        } else if (correctWord.includes(guessChar)) {
-          responseObj.result[i] = "W";
-          correctWord = correctWord.replace(guessChar, "-");
-        } else {
-          responseObj.result[i] = "-";
-        }
-      }
+//         if (guessChar === correctChar) {
+//           responseObj.result[i] = "C";
+//         } else if (correctWord.includes(guessChar)) {
+//           responseObj.result[i] = "W";
+//           correctWord = correctWord.replace(guessChar, "-");
+//         } else {
+//           responseObj.result[i] = "-";
+//         }
+//       }
 
-      responseObj.allCorrect =
-        req.params.word.toUpperCase() === correctWord.toUpperCase();
-    }
+//       responseObj.allCorrect =
+//         req.params.word.toUpperCase() === correctWord.toUpperCase();
+//     }
 
-    res.json(responseObj);
-  } catch (error) {
-    if (error === false) {
-      res.status(200).json({ success: false, error: "Word not allowed" });
-    } else {
-      console.error("Error processing allowed word:", error);
-      res.status(500).json({ success: false, error: "Internal server error" });
-    }
-  }
-});
+//     res.json(responseObj);
+//   } catch (error) {
+//     if (error === false) {
+//       res.status(200).json({ success: false, error: "Word not allowed" });
+//     } else {
+//       console.error("Error processing allowed word:", error);
+//       res.status(500).json({ success: false, error: "Internal server error" });
+//     }
+//   }
+// });
 
-cron.schedule(
-  "0 5 * * *",
-  async () => {
-    console.log("Running new round cron job");
-    // const Round = await newRound();
-    // console.log(Round);
-  },
-  {
-    scheduled: true,
-    timezone: "Europe/Stockholm",
-  }
-);
+// cron.schedule(
+//   "0 5 * * *",
+//   async () => {
+//     console.log("Running new round cron job");
+//     // const Round = await newRound();
+//     // console.log(Round);
+//   },
+//   {
+//     scheduled: true,
+//     timezone: "Europe/Stockholm",
+//   }
+// );
 
-app.get("/api/test", async (req, res) => {
-  console.log("GET /api/test");
+// app.get("/api/test", async (req, res) => {
+//   console.log("GET /api/test");
 
-  let text = await newRound();
+//   let text = await newRound();
 
-  res.json(text);
-});
+//   res.json(text);
+// });
 
-app.get("cron", async (req, res) => {
-  console.log("Vercel cron test");
-  res.json({ success: true });
-});
+// app.get("cron", async (req, res) => {
+//   console.log("Vercel cron test");
+//   res.json({ success: true });
+// });
 
-module.exports = app;
+// module.exports = app;
